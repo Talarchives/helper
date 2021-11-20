10ffc8714252205b3d57ce292c0b5676998e5311
Talal <talalahmad257@gmail.com>
2021-11-20 06:14:49 +0500

    Setup command handler

--- src/index.js
+++ src/index.js
@@ -1,10 +1,47 @@
 const
-  { Client, Intents } = require('discord.js'),
+  { Client, Intents, Collection } = require('discord.js'),
   { TOKEN } = process.env,
-  client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+  client = new Client({ intents: [Intents.FLAGS.GUILDS] }),
+
+  fs = require('fs'), path = require('path'),
+  commands = readdirRecursive('./src/commands');
+
+client.commands = new Collection();
+for (const file of commands) {
+  const command = require(`../${file}`);
+  client.commands.set(command.name, command);
+}
 
 client.on('ready', () => {
   console.log('ready');
 });
 
+client.on('interactionCreate', interaction => {
+  try {
+    client.commands.get(interaction.commandName).run(client, interaction);
+  } catch (e) {
+    console.log(e);
+  }
+});
+
 client.login(TOKEN);
+
+function readdirRecursive(directory) {
+  const result = [];
+
+  (function read(dir) {
+    const files = fs.readdirSync(dir);
+
+    for (const file of files) {
+      const filepath = path.join(dir, file);
+
+      if (fs.statSync(filepath).isDirectory()) {
+        read(filepath);
+      } else {
+        result.push(filepath.replace(/\\/g, '/'));
+      }
+    }
+  }(directory));
+
+  return result;
+}
