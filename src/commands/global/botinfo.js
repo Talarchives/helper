2848aa1ee7ac602d0545ac419983c3078fb3fb03
Talal <talalahmad257@gmail.com>
2021-11-22 13:04:17 +0500

    Added actual bot info

--- src/commands/global/botinfo.js
+++ src/commands/global/botinfo.js
@@ -3,7 +3,16 @@ module.exports = {
     name: 'botinfo',
     description: 'Shows information about the bot.'
   },
-  run(client, interaction) {
-    interaction.reply({ ephemeral: true, content: 'Stuff about the bot' });
+  async run(client, interaction) {
+    const content = [
+      `**Bot Name:** ${client.user.tag}`,
+      `**Bot Owner:** ${(await client.users.fetch(process.env.OWNERID)).username}`,
+      `**Bot Uptime:** ${client.uptime}`,
+      `**Bot Guilds:** ${client.guilds.cache.size}`,
+      `**Bot Channels:** ${client.channels.cache.size}`,
+      `**Bot Users:** ${client.users.cache.size}`,
+      `**Bot Commands:** ${client.commands.size}`
+    ].join('\n');
+    interaction.reply({ ephemeral: true, content });
   }
 };
